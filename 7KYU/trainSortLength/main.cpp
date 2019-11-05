bb0dd9642f6904b7fd52a31ec581788688c12a9a
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-11-05 17:32:36 +0100

    sort length complete

--- 7KYU/trainSortLength/main.cpp
+++ 7KYU/trainSortLength/main.cpp
@@ -1,10 +1,46 @@
 #include<iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+
+
+
+
+bool compare(string &s1,string &s2) 
+{ 
+    return s1.size() < s2.size(); 
+} 
+
+
+
 
 int main () {
 
+vector <string> array{ "cheese", "fruit", "wine", "egg", "spagetti"};
 
+    for (int i =0; i < array.size(); i++) {
+
+   
+        cout<< array[i] << " ";
+
+
+    }
+
+
+cout << endl;
+
+
+
+sort(array.begin(),array.end(),compare);
+
+
+    for (int i =0; i < array.size(); i++) {
+
+        cout<< array[i] << " ";
+
+
+    }
 
 
     return 0;
