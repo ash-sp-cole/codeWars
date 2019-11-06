57ccb9cfbf449e16d473915001fa2e22f6f2e4f9
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-11-06 15:58:42 +0100

    work in progress

---
+++ 6KYU/mexicanWave/main.cpp
@@ -0,0 +1,35 @@
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<iostream>
+#include<sstream>
+#include <cctype>
+using namespace std;
+
+int main () {
+
+string str = "hapoppyboi";
+string word{};
+
+
+;
+
+vector <string> answer{};
+char k = {};
+
+ // k = toupper(k);
+
+
+
+for (int  i = 0 ; i < str.size(); i++) {
+
+str[i] = toupper(str[i]);
+
+
+}
+
+answer.push_back(word);
+cout << answer.at(0);
+
+    return 0;
+}
