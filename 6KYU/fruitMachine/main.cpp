55520f81b86a1687c2b85cef8c894d37c7e5b2c5
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-11-10 16:38:23 +0100

    g

--- 6KYU/fruitMachine/main.cpp
+++ 6KYU/fruitMachine/main.cpp
@@ -10,6 +10,22 @@ using namespace std;
 int main () {
 
 
-cout << "hellow world";
+
+vector <vector<string>> reels {
+        reels[0] = {"Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack"},
+        reels[1] = {"Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack"},
+        reels[2] = {"Wild", "Star", "Bell", "Shell", "Seven", "Cherry", "Bar", "King", "Queen", "Jack"}
+};
+
+vector <int> spins (3);
+
+for  (int i =0; i <spins.size();i++) {
+cin >> spins[i];
+
+}
+
+
+
+cout << spins.at(0) << spins.at(1);
 return 0;
 }
