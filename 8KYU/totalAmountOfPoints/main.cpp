eff44cf5d8f60b773d9b502704cb5392db7c8d12
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-01 15:52:45 +0100

    complete

--- 8KYU/totalAmountOfPoints/main.cpp
+++ 8KYU/totalAmountOfPoints/main.cpp
@@ -1,3 +1,22 @@
 #include<iostream>
-
+#include<vector>
+#include <algorithm>
+#include <numeric>      // std::accumulate
 using namespace std; 
+
+int main () {
+
+vector <int> group1 { 1,4,2};
+vector <int> group2 { 5,4,3};
+
+
+ int totalOne = accumulate(group1.begin(),group1.end(),0);
+
+ int totalTwo = accumulate(group1.begin(),group1.end(),0);
+
+int finalTotal = totalOne + totalTwo;
+
+
+cout <<  finalTotal;
+    return 0;
+}
