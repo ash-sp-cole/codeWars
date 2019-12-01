76b199c9d67d437c6308cb50c3ca24f2f8602246
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-01 15:06:11 +0100

    complete

--- 8KYU/sortAndStar/main.cpp
+++ 8KYU/sortAndStar/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<algorithm>
 
 using namespace std;
 
@@ -9,14 +10,26 @@ using namespace std;
 int main () {
 
 
+string answer{"test"};
+
+
+string word {"550011051"};
+
+for (int i =0; i < word.size(); i++) {
+
+char k = word[i];
+
+if (k == '0') { k = 'O'; }
+else if ( k == '5') { k = 'S';}
+else if ( k == '1') { k = 'I';}
 
 
 
+answer = answer + k;
+}
 
 
-
-
-
+cout << answer;
 
 
 
