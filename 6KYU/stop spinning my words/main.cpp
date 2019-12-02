69c720edd76077d76e1d4a5537dd3235577c63d5
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-02 18:26:25 +0100

    work on strings

--- 6KYU/stop spinning my words/main.cpp
+++ 6KYU/stop spinning my words/main.cpp
@@ -1,25 +1,57 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 
 int main () {
 
+string str {} ,tmp{};
+string reverseStr{};
+vector <string> wordList {};
 
 
+cout << "enter word" << endl ;
+
+cin >> str;
+int size = str.size();
 
 
+stringstream s(str);
+string word{};
 
+while (s >> word) wordList.push_back(word);
 
-
-
-cout << " test";
-
+for (int i=0; i < wordList.size(); i++) {
+    cout << wordList[i];
+}
 
 
 
     return 0;
 }
+
+
+
+
+
+// if ( str.size() >=5 ) {
+
+// for (int i = str.size()-1; i >=0; i--) 
+// {
+//     k = str[i];
+
+//     reverseStr.push_back(k);
+
+
+// }
+
+// cout << reverseStr;
+
+// }
+// else {
+// cout << str;
+// }
