610ead77673e308c944e5307cbae78495ddc9c8b
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-12-29 17:55:45 +0100

    reading grade level detector start up cpp files

---
+++ practiceFolder/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+string reverse(string name)
+{
+
+    string answer = "test Reverse";
+
+    return answer;
+}
+
+int main(void)
+{
+
+    string name = "Hello World\n";
+    cout << name;
+    string answer = reverse(name);
+    cout << answer;
+    return 0;
+}
