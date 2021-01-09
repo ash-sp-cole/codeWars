6cb8de1b338e0b2ef342af8d7c434fe0e97b4c4a
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2021-01-09 15:25:55 +0100

    update javascript cleetcide

--- readingGradeDetector/main.cpp
+++ readingGradeDetector/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
-
+#include <vector>
 
 using namespace std;
 
+int main(void)
+{
 
-int main (void) {
+    char testString[] = "Hello world test test test test test";
+    vector<int> array{};
+    size_t n = sizeof(testString) / sizeof(testString[0]);
 
-    cout << "Working read";
+    int wordCount = 0;
 
+    for (int i = 0; i < n; i++){
+
+        if (testString[i] != ' ' && testString[i-1] == ' ' || testString[i] != ' ' && i == 0){
+            wordCount++
+;
+        }
+
+
+    }
+cout << wordCount;
     return 0;
 }
+
