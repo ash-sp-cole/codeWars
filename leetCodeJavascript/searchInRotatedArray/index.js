d882c2ce2640f71d176204b609e8ca3ccae498ee
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2021-01-12 15:46:57 +0100

    update

--- leetCodeJavascript/searchInRotatedArray/index.js
+++ leetCodeJavascript/searchInRotatedArray/index.js
@@ -7,22 +7,6 @@ let target = 5;
 
 while ( low <= high ){
 
-    let mid = (low + high) / 2;
-   
-    if ( arr[mid] === target){
-
-        console.log(mid)
-    
-    }
-    if (arr[mid] > target){
-        console.log("test else if hgih")
-        high = mid-1
-    }
-    else{
-        console.log("test else if" , [mid])
-        low = mid + 1
-    }
-
 }
 
 
